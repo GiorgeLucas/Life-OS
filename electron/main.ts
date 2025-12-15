61a160489e32cd2fc742765f4e6384404ff3df53
Giorge Lucas <giorgelucasx@gmail.com>
2025-12-15 19:39:01 -0300

    feat(ui/db/dev): add basic preload script and setup an environment for a future database integration
    - create a simple preload.js script
    - connect preload.js to main.ts
    - add global window types file

--- electron/main.ts
+++ electron/main.ts
@@ -1,7 +1,10 @@
-import { app, BrowserWindow } from 'electron';
-import path from 'path';
+import { app, BrowserWindow } from "electron";
+import { fileURLToPath } from "url";
+import path from "path";
 
 const isDev = !app.isPackaged;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -10,19 +13,22 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
     },
   });
 
   if (isDev) {
-    win.loadURL('http://localhost:5173');
+    win.loadURL("http://localhost:5173");
     win.webContents.openDevTools();
   } else {
-    win.loadFile(path.join(__dirname, '../dist/index.html'));
+    win.loadFile(
+      path.join(__dirname, "../dist/index.html"),
+    );
   }
 }
 
 app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
 });
